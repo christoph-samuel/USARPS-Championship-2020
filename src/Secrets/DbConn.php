b6902300f05363a666680afe062ad5179341bdf4
Christoph Samuel <chsamuel@gmx.at>
2021-06-12 23:37:48 +0200

    M7-02-ADV: User Story 1

---
+++ src/Secrets/DbConn.php
@@ -0,0 +1,16 @@
+<?php
+
+// ******* TODO ******* Fill in the '????' and rename this file to 'DbConn.php'
+
+namespace Secrets;
+
+class DbConn
+{
+    public const PARAMS = [
+        'driver' => 'mysqli',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'usarps_db_orm',
+        'host' => 'localhost'
+    ];
+}
